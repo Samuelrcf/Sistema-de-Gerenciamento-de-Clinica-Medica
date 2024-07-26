5c94821aeed0c7d53d6c03e97a1d7dbc9c2f3c35
Samuel Rógenes <rogenescarvalho@hotmail.com>
2024-07-26 14:08:21 -0300

    adding exceptions

---
+++ src/main/java/com/samuelrogenes/clinicmanagement/exceptions/ConflictException.java
@@ -0,0 +1,11 @@
+package com.samuelrogenes.clinicmanagement.exceptions;
+
+public class ConflictException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ConflictException(String message) {
+		super(message);
+	} 
+
+}
